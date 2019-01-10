d4e28ff37d711b3c40a4d67335e703e95b6ad5f5
Beier22 <43845116+Beier22@users.noreply.github.com>
2019-01-10 10:42:22 +0100

    View and Controller

--- src/moviecollection/gui/controller/AddEditViewController.java
+++ src/moviecollection/gui/controller/AddEditViewController.java
@@ -7,7 +7,14 @@ package moviecollection.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import moviecollection.be.Category;
 
 /**
  * FXML Controller class
@@ -16,6 +23,15 @@ import javafx.fxml.Initializable;
  */
 public class AddEditViewController implements Initializable {
 
+    @FXML
+    private TextField movieTitle;
+    @FXML
+    private TextField movieYear;
+    @FXML
+    private ListView<Category> movieCat;
+    @FXML
+    private Button saveButton;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +39,21 @@ public class AddEditViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void pressCancel(ActionEvent event) {
+        Stage st = (Stage) saveButton.getScene().getWindow();
+        st.close();
+    }
+
+    @FXML
+    private void pressSave(ActionEvent event) {
+        
+        //add shit
+        
+        
+        Stage st = (Stage) saveButton.getScene().getWindow();
+        st.close();
+    }
     
 }
